10a939e33b9354b54069fbded0087c22227cadc8
Stephanie <stephanieanderson5133@gmail.com>
2022-01-28 16:15:22 -0600

    time coloring complete, local storage started

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,38 @@
 var btn8= $('#btn8');
 var data8=$('#data8');
+var timeContainer = $('.timeContainer')
+
 //add today's date to top
 var today = new Date();
 $("#currentDay").html(today);
+var currentTime = today.getHours();
+//color code for date
+timeContainer.each(function(index, element){
+  var timeBlock = element.id.split("-")[0] 
+  console.log(timeBlock, currentTime)
+  //current
+  if (timeBlock == currentTime)  {
+    $(element).css({backgroundColor: 'blue'});
+   }
+   //future
+   else if (timeBlock < currentTime)  {
+    $(element).css({backgroundColor: 'yellow'});
+   }
+   //past
+   else {
+    $(element).css({backgroundColor: 'gray'});
 
+   }
 
-// create if then statement for times and colors
-console.log(today.getHours());
-
-//if attempt
- if (today.getHours() > 8)  {
- $("#8container").css({backgroundColor: 'red'});
-}
-if (today.getHours() > 9)  {
-  $("#9container").css({backgroundColor: 'red'});
- } 
-
+})
 
  //save to local storage
 
 btn8.on('click', function(event) {
 event.preventDefault();
-localStorage.content = data8.html();
-console.log(localStorage);
+localStorage.setItem("data8", data8.val());
+console.log(data8.val());
 });
  // #data8 and #btn8
+
+data8.val(localStorage.getItem("data8"));
