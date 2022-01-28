6ef1c9d7560f327e24bb417e79165ec52156da0b
Stephanie <stephanieanderson5133@gmail.com>
2022-01-28 15:25:34 -0600

    highlighter works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,27 @@
+var btn8= $('#btn8');
+var data8=$('#data8');
 //add today's date to top
 var today = new Date();
 $("#currentDay").html(today);
 
 
 // create if then statement for times and colors
+console.log(today.getHours());
 
-//save to local storage
+//if attempt
+ if (today.getHours() > 8)  {
+ $("#8container").css({backgroundColor: 'red'});
+}
+if (today.getHours() > 9)  {
+  $("#9container").css({backgroundColor: 'red'});
+ } 
 
+
+ //save to local storage
+
+btn8.on('click', function(event) {
+event.preventDefault();
+localStorage.content = data8.html();
+console.log(localStorage);
+});
+ // #data8 and #btn8
